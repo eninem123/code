8132a2c86658af697212a997d22dcd403b402d38
eninem123 <445003409@qq.com>
2018-12-05 13:03:07 +0800

    增加两个微信公众号爬虫，一个利用个人公众号爬，一个利用抓包软件charles

---
+++ wechatspider1.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+import requests
+import time
+import csv
+"""
+微信公众号爬虫
+需要提交的data
+以下个别字段是否一定需要还未验证。
+注意修改yourtoken,number
+begin表示从第begin页开始爬取，为5的倍数，从0开始。如0、5、10……
+token可以使用Chrome自带的工具进行获取
+fakeid是公众号独一无二的一个id，等同于后面的__biz
+"""
+
+
+headers = {
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36",
+    "Host": "mp.weixin.qq.com",
+    "Referer": "https://mp.weixin.qq.com/cgi-bin/appmsg?t=media/appmsg_edit&action=edit&type=10&isMul=1&isNew=1&lang=zh_CN&token=1862390040",
+    "Cookie": "pgv_pvid=9498751209; pgv_pvi=428076032; RK=sPjsyLbhy6; ptcz=9859bf94715699d49e4a6a66b8c0d499cf5154865c8a114a0701f84e7d8baae6; o_cookie=2442972114; pac_uid=1_2442972114; ua_id=0yISQiLv4q7caT0BAAAAAK03YmkztOT6jPo1WZx2FDA=; mm_lang=zh_CN; noticeLoginFlag=1; _ga=GA1.2.1956877728.1541494631; pgv_si=s3467182080; cert=tPEtxRtBtxEVOdtKgSvvJttAj9P5tCum; master_key=P79GlwMcDCH8mTm9v0HinqJD1yl2MgrQLUgSJdX7U1c=; pgv_info=ssid=s4831455345; ptisp=cm; ptui_loginuin=1105499328; pt2gguin=o1105499328; uin=o1105499328; skey=@sbzf0jRRU; sig=h019d6b67ebaf8cd117a515adb82fc9d7f4208c13fff66d3fdebd77aa705aadd5e50cc08770ea07a7c0; uuid=7c6a0a76298cb2dce3a970e6aa0405c1; ticket=fd7eadf4120f0db64fdc431dbc5086b74cd40477; ticket_id=gh_0c2e05940f96; remember_acct=1105499328%40qq.com; data_bizuin=3240103118; bizuin=3268085583; data_ticket=dr6qgVEjHOGDrDTUP5lEmRfTRyCjOzvVRNAauMMCpkvlqCa21CVkF0AzUoa+MqEg; slave_sid=dlRYQ0xYdmFCczJxT3ZYNDJWTUdGVW9Qd0RTZEFFMUZjRkNsUEZIWWJ6TXBaTk5BQ0NmcHd0RjBFeFc3RDRnejk1aEJZVlhLbUJVMnVjWEl3REMyR3pOTFdJUFB1RElqbV9hR2ZDSEc2WF9vT0NhUVhLZWp3UGJGZ0RHdmk0enFMN0NpWXlBSlppVXdubFY0; slave_user=gh_0c2e05940f96; xid=4f0f831250e59fc989580c2eac5f3b8e; openid2ticket_ozgwgwduTYsDT5nndZC_6BVIArXw=VsyuVLTuTIi7OFhL06Bikut7OzsKmGHfBVs6kinZgvM="
+}
+
+for i in range(11):
+    url = "https://mp.weixin.qq.com/cgi-bin/appmsg?token=1687199925&lang=zh_CN&f=json&ajax=1&random=0.5124554479638495&action=list_ex&begin={}&count=5&query=&fakeid=MjM5NDYwMzUyMA%3D%3D&type=9".format(
+        str(i * 5))
+    print(i)
+    #url = "https://mp.weixin.qq.com/cgi-bin/appmsg?token=1862390040&lang=zh_CN&f=json&ajax=1&random=0.17794584803309532&action=list_ex&begin={}&count=5&query=&fakeid=&type=9".format(
+     #   str(i * 5))
+
+    response = requests.get(url, headers=headers)
+
+    content_json = response.json()
+    for item in content_json["app_msg_list"]:
+
+    # 遍历 构造可存储字符串
+
+        f = open('美年大健康.csv', 'a+', encoding='utf-8-sig', newline='')
+        writer = csv.writer(f)
+        li = []
+        title = item["title"]
+        url = item["link"]
+
+        t = (title,url)
+
+        li.append(t)
+        print(li)
+        writer.writerows(li)
+
---
+++ wechatspider2.py
@@ -0,0 +1,79 @@
+import requests
+import time
+import json,csv
+"""
+抓包软件爬微信公众号文章，不容易封的方法
+爬取步骤
+1。微信客户端打开历史文章，charles抓包，不断下滚翻下一页
+2。抓包软件看见一个mp.weixin.qq.com不断变黄色，打开看里面的url
+3。浏览器打开url就是，json包了
+4。f12查headers拼接进来，url的offset从0开始翻页，0，11，21，31
+"""
+
+
+class mp_spider(object):
+
+    def __init__(self):
+        self.li = []
+        self.offset = 0
+        self.count = 0
+        self.base_url = 'https://mp.weixin.qq.com/mp/profile_ext?action=getmsg&__biz=MjM5NzI3NDg4MA==&f=json&offset=20&count=10&is_ok=1&scene=124&uin=MTc5NzUxMjM0MA%3D%3D&key=94f4a4917bbfd6eb461d9f5aa8f024110599a6e69dcfd72fa14ce301213e18517881ca1c58d2cece7fbdc7de7aad706a36cde33d82ad49f89a0dea156e00f09de705a15a1e9c86a07b4293472edb126e&pass_ticket=hvCRpWOU%2FTny4nf72ZRSuf14cQaFESs1fEeBmhaMrxhtAQfxTVnOZtS32sxFlkiJ&wxtoken=&appmsg_token=985_wXVINAHkUx6nhdmsUPj8Wqo9unuC3MOzZg22gw~~&x5=0&f=json'
+        self.headers = {
+            'Host': 'mp.weixin.qq.com',
+            'Connection': 'keep-alive',
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.110 Safari/537.36',
+            'Cookie': 'pgv_pvid=6751777100; sd_userid=85871542339048696; sd_cookie_crttime=1542339048696; pgv_pvi=1805300736; pt2gguin=o2442972114; RK=sPjsyLbxm6; ptcz=56838912653767a7f02cde6edf3f843004786c14270f8f3137cf852accde6186; wxuin=1797512340; pass_ticket=hvCRpWOU/Tny4nf72ZRSuf14cQaFESs1fEeBmhaMrxhtAQfxTVnOZtS32sxFlkiJ; devicetype=iMacMacBookPro81OSXOSX10.12.6build(16G29); version=12031210; lang=pt; rewardsn=; wxtokenkey=777; wap_sid2=CJS5j9kGElxPcTFUU2FVWVVFZTNfUDktMzBpNWFXVE0xZTVyZXlVampQN3ZtOTA3blpVa25sc0ZUMU55b01scFlrc3d4anMyYTN6XzV6bjAwcHNaWXhmcTE1MC1QZGtEQUFBfjCSlZ3gBTgNQJVO'
+        }
+
+    def request_data(self):
+        response = requests.get(self.base_url.format(self.offset), headers=self.headers)
+        if 200 == response.status_code:
+            self.parse_data(response.text)
+
+    def parse_data(self, response_data):
+
+        all_datas = json.loads(response_data)
+
+        if 0 == all_datas['ret']:
+            if 1 == all_datas['can_msg_continue']:
+                summy_datas = all_datas['general_msg_list']
+                datas = json.loads(summy_datas)['list']
+                for data in datas:
+                    try:
+                        title = data['app_msg_ext_info']['title']
+                        title_child = data['app_msg_ext_info']['digest']
+                        article_url = data['app_msg_ext_info']['content_url']
+                        cover = data['app_msg_ext_info']['cover']
+                        copyright = data['app_msg_ext_info']['copyright_stat']
+                        copyright = '原创文章_' if copyright == 11 else '非原创文章_'
+                        self.count = self.count + 1
+                        print('第【{}】篇文章'.format(self.count), copyright, title, title_child, article_url, cover)
+                        # 保存成csv
+                        f = open('人民网.csv', 'w', encoding='utf-8-sig', newline='')
+                        writer = csv.writer(f)
+                        # 将一个个字段数据提取出来
+                        # print(title)
+                        t = (title, title_child, article_url,cover)
+                        self.li.append(t)
+                        # 列表格式 ：[('xxxx',), ('xxxx',)]
+                        # print(self.li)
+                        # writerows保存多行，格式是[('xxxx',), ('xxxx',)] writerow保存单行，格式['a','b']
+                        writer.writerows(self.li)
+                    except:
+                        continue
+
+                time.sleep(3)
+                self.offset = all_datas['next_offset']  # 下一页的偏移量
+                self.request_data()
+            else:
+                exit('数据抓取完毕！')
+        else:
+            exit('数据抓取出错:' + all_datas['errmsg'])
+
+
+
+
+if __name__ == '__main__':
+    d = mp_spider()
+    d.request_data()
